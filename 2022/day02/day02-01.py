05f824919ea19f0c0edb2ad9b41aaea1a67d0d5c
Ricardo Augusto Ripoll Vieira <ricardo@rvieira.co.uk>
2022-12-02 15:52:36 +0100

    Bug fixed, second column being used. 	modified:   day02-01.py

--- 2022/day02/day02-01.py
+++ 2022/day02/day02-01.py
@@ -2,27 +2,27 @@ totalscore=score=i=0
 for line in open('/Users/ricardovieira/github/adventofcode/2022/day02/input.txt'):
     score=0
     i+=1
-    if line.split()[0] == "A": # Rock
+    if line.split()[1] == "X": # Rock
         score+=1
-        if line.split()[1] == "X": # Rock
+        if line.split()[0] == "A": # Rock
             score+=3
-        elif line.split()[1] == "Y": # Paper
+        elif line.split()[0] == "B": # Paper
             score+=0
         else: # Scissor
             score+=6
-    elif line.split()[0] == "B": # Paper
+    elif line.split()[1] == "Y": # Paper
         score+=2
-        if line.split()[1] == "X": #Rock
+        if line.split()[0] == "A": #Rock
             score+=6
-        elif line.split()[1] == "Y": # Paper
+        elif line.split()[0] == "B": # Paper
             score+=3
         else: # Scissor
             score+=0
     else: # Scissor
         score+=3
-        if line.split()[1] == "X": #Rock
+        if line.split()[0] == "A": #Rock
             score+=0
-        elif line.split()[1] == "Y": # Paper
+        elif line.split()[0] == "B": # Paper
             score+=6
         else: # Scissor
             score+=3
